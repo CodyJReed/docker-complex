f6f49fb25890e3e72495d2cf91054fd1519c3c49
CodyJReed <cody2j2reed@gmail.com>
2020-01-04 01:57:58 -0500

    Refactor on fib function

--- worker/index.js
+++ worker/index.js
@@ -8,10 +8,7 @@ const redisClient = redis.createClient({
 });
 const sub = redisClient.duplicate();
 
-function fib(index) {
-  if (index < 2) return 1;
-  return fib(index - 1) + fib(index - 2);
-}
+const fib = index => (index < 2 ? 1 : fib(index - 1) + fib(index - 2));
 
 sub.on("message", (channel, message) =>
   redisClient.hset("values", message, fib(parseInt(message)))
